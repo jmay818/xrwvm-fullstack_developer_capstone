913b2a717f094830016d2afee39a546082721ff3
J May <jmay818@gmail.com>
2024-09-09 14:20:24 -0400

    Update site

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     # path for get cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for get dealers
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for get dealer details
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
