7ffff100096b39262c4f922faf9083d24d197537
J May <jmay818@gmail.com>
2024-09-10 20:54:35 -0700

    Update site

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,12 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://jmay818-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://jmay818-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost',
+                 'https://jmay818-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                 '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://jmay818-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -94,7 +98,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        '''django.contrib.auth.password_validation.
+        UserAttributeSimilarityValidator''',
     },
     {
         'NAME':
